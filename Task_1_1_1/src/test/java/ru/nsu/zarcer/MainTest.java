50abaa3efbcc403405e35c25403d786cfce54e09
Zarcer <rorer232323@gmail.com>
2024-09-09 16:57:32 +0700

    sample_2

--- Task_1_1_1/src/test/java/ru/nsu/zarcer/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/zarcer/MainTest.java
@@ -9,4 +9,14 @@ class MainTest {
     void SampleTest() {
         assertEquals(3, Main.sum(1, 2));
     }
+
+    @Test
+    void ZeroTest() {
+        assertEquals(0, Main.sum(0, 0));
+    }
+
+    @Test
+    void MinusTest() {
+        assertEquals(-5, Main.sum(-5, 0));
+    }
 }
