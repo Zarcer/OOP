ed3371456d73261093724bf8ebfabbe0002e18b4
Zarcer <rorer232323@gmail.com>
2024-09-11 19:27:38 +0700

    v6

--- Task_1_1_1/src/main/java/ru/nsu/zarcer/HeapSort.java
+++ Task_1_1_1/src/main/java/ru/nsu/zarcer/HeapSort.java
@@ -1,12 +1,12 @@
 package ru.nsu.zarcer;
 
 public class HeapSort {
-    public static void heapsort (int[] arr) {
+    public static void heapsort(int[] arr) {
         int n = arr.length;
-        for(int i = n/2-1; i>=0;i--) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
         }
-        for(int i = n-1;i>=0;i--) {
+        for (int i = n - 1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
@@ -14,17 +14,17 @@ public class HeapSort {
         }
     }
 
-    static void heapify (int arr[], int n, int i) {
+    static void heapify(int arr[], int n, int i) {
         int largest = i;
-        int left = 2*i + 1;
-        int right = 2*i + 2;
-        if(left<n && arr[left] > arr[largest]) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest]) {
             largest = left;
         }
-        if(right<n && arr[right] > arr[largest]) {
+        if (right < n && arr[right] > arr[largest]) {
             largest = right;
         }
-        if(largest != i) {
+        if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
@@ -33,17 +33,16 @@ public class HeapSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = new int[] {5, 4, 3, 2, 1};
+        int arr[] = new int[]{5, 4, 3, 2, 1};
         int n = arr.length;
         heapsort(arr);
         System.out.print("[");
-        for(int i = 0;i<n;i++)
-        {
-            if(i == n-1) {
-                System.out.print(arr[i]+"]");
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                System.out.print(arr[i] + "]");
                 break;
             }
-            System.out.print(arr[i]+", ");
+            System.out.print(arr[i] + ", ");
         }
     }
 }
