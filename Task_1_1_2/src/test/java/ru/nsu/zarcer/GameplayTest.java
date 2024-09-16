8985f104b856a4ee4766da3afa9bc54f703fe0f0
Zarc <rorer232323@gmail.com>
2024-09-16 20:12:22 +0700

    v4

--- Task_1_1_2/src/test/java/ru/nsu/zarcer/GameplayTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/zarcer/GameplayTest.java
@@ -10,32 +10,5 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameplayTest {
-    private String runGame(String input) {
-        InputStream inputStream = System.in;
-        PrintStream outputStream = System.out;
-        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-        Gameplay.main(null);
-        System.setIn(inputStream);
-        System.setOut(outputStream);
-        return myOut.toString();
-    }
 
-
-    @Test
-    void refresher() {
-        int round_start_number = Gameplay.round_count[0];
-        assertEquals(1, Gameplay.round_count[0]);
-        runGame("0 -1");
-        int round_next = Gameplay.round_count[0];
-        assertEquals(2, Gameplay.round_count[0]);
-    }
-
-    @Test
-    void maintest() {
-        runGame("0 -1");
-        assertTrue(true);
-    }
 }
