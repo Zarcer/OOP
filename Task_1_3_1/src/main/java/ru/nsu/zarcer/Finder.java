a70c14d3b07e467b5ef1124b430ff8f7d72129a4
Letov Alexey <a.letov@g.nsu.ru>
2024-10-12 23:57:21 +0700

    v6

--- Task_1_3_1/src/main/java/ru/nsu/zarcer/Finder.java
+++ Task_1_3_1/src/main/java/ru/nsu/zarcer/Finder.java
@@ -1,58 +1,89 @@
 package ru.nsu.zarcer;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
+/**
+ * Finder class overall.
+ */
 public class Finder {
+
+    /**Find substring in string, returns list of indexes.
+     *
+     * @param readerr reader stream, goes through shell methods
+     *
+     * @param subName substring
+     *
+     * @return list of indexes
+     *
+     * @throws IOException throws exception without catching
+     *
+     */
     private static ArrayList<Integer> find(Reader readerr, String subName) throws IOException {
         int length = subName.length();
         ArrayList<Integer> output = new ArrayList<>();
-        int c;
-        int indexRead=-1;
-        while((c=readerr.read())!=-1){
-            int i = 0;
-            indexRead++;
-            int startingIndex=0;
-            while(subName.charAt(i)==(char)c){
-                if(i==0){
-                    startingIndex=indexRead;
+        char[] buffer = new char[length];
+        int charRead;
+        if ((charRead = readerr.read(buffer)) != -1) {
+            int indexRead = charRead - 1;
+            String temp = new String(buffer);
+            while (true) {
+                if (temp.indexOf(subName) != -1) {
+                    output.add(indexRead - length + 1);
                 }
-                if(i==length-1){
-                    output.add(startingIndex);
+                charRead = readerr.read();
+                indexRead++;
+                if (charRead == -1) {
                     break;
                 }
-                else{
-                    i++;
-                }
-                if((c=readerr.read())==-1){
-                    readerr.close();
-                    return output;
-                }
-                indexRead++;
+                buffer[length - 1] = (char) charRead;
+                temp = temp.substring(1) + buffer[length - 1];
             }
         }
-        readerr.close();
         return output;
     }
 
+    /**Shell method for find, uses files.
+     *
+     * @param fileName name of file
+     *
+     * @param subName substring
+     *
+     * @return list of indexes
+     *
+     * @throws IOException rethrow exception
+     *
+     */
     public static ArrayList<Integer> findFile(String fileName, String subName) throws IOException {
-        try(BufferedReader br = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8))){
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8))) {
             return find(br, subName);
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             throw e;
         }
 
     }
 
+    /**Shell method for find using resources.
+     *
+     * @param fileName name of file
+     *
+     * @param subName substring
+     *
+     * @return list of indexes
+     *
+     * @throws IOException rethrow exception
+     *
+     */
     public static ArrayList<Integer> findRecourse(String fileName, String subName) throws IOException {
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(Finder.class.getResourceAsStream("/"+fileName), StandardCharsets.UTF_8))){
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Finder.class.getResourceAsStream("/" + fileName), StandardCharsets.UTF_8))) {
             return find(br, subName);
         } catch (IOException e) {
             throw e;
         }
     }
-
-
 }
--- Task_1_3_1/src/test/java/ru/nsu/zarcer/FinderTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/zarcer/FinderTest.java
@@ -2,48 +2,75 @@ package ru.nsu.zarcer;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FinderTest {
     @Test
-    public void findResourceTest(){
-        try{
+    public void findResourceTest() {
+        try {
             ArrayList<Integer> test = Finder.findRecourse("test.txt", "⌨");
             ArrayList<Integer> validation = new ArrayList<>();
             validation.add(1);
             validation.add(8);
             assertEquals(test, validation);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    public void findFileTest(){
-        File testFile = new File("bigTest.txt");
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(testFile, StandardCharsets.UTF_8))){
+    public void substringVoidTest() {
+        try {
+            ArrayList<Integer> test = Finder.findRecourse("test.txt", "тыква");
             ArrayList<Integer> validation = new ArrayList<>();
-            testFile.createNewFile();;
+            assertEquals(test, validation);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void substringEqualsTest() {
+        try {
+            ArrayList<Integer> test = Finder.findRecourse("equals.txt", "абра");
+            ArrayList<Integer> validation = new ArrayList<>();
+            validation.add(0);
+            validation.add(3);
+            assertEquals(test, validation);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void findFileTest() {
+        File testFile = new File("bigTest.txt");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(testFile, StandardCharsets.UTF_8))) {
+            ArrayList<Integer> validation = new ArrayList<>();
+            testFile.createNewFile();
+            ;
             String input = "абракадабра";
             String subName = "бра";
             int shift = 0;
-            for(int i = 0;i<1000000;i++){
+            for (int i = 0; i < 1000000; i++) {
                 bw.write(input);
                 bw.flush();
-                validation.add(1+shift);
-                validation.add(8+shift);
-                shift = shift+11;
+                validation.add(1 + shift);
+                validation.add(8 + shift);
+                shift = shift + 11;
             }
             ArrayList<Integer> testing = Finder.findFile("bigTest.txt", subName);
             assertEquals(validation, testing);
             testFile.delete();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             testFile.delete();
             System.out.println(e.getMessage());
         }
