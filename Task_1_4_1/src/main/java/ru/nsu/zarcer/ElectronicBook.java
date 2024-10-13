e3b0e5a252dfbfb7e63b6cbff36fbc8ceb539b46
Zarc <rorer232323@gmail.com>
2024-10-14 01:32:40 +0700

    v4

--- Task_1_4_1/src/main/java/ru/nsu/zarcer/ElectronicBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/zarcer/ElectronicBook.java
@@ -3,9 +3,15 @@ package ru.nsu.zarcer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class of book itself.
+ */
 public class ElectronicBook {
     private List<Semester> semesters;
 
+    /**
+     * Constructor with restrictions per control and semester.
+     */
     ElectronicBook() {
         semesters = new ArrayList<>();
         semesters.add(new Semester(2, 3, 1, 3, 3, 3, 0, 0));
@@ -18,29 +24,68 @@ public class ElectronicBook {
         semesters.add(new Semester(0, 0, 0, 0, 0, 0, 0, 1));
     }
 
+    /**Adds grade to book.
+     *
+     * @param semesterNumber int number of semester
+     *
+     * @param grade from 1 to 5
+     *
+     * @param type type of assignment
+     *
+     * @param subject type of subject
+     *
+     * @return true if successful, false otherwise
+     *
+     */
     public boolean addGradeBook(int semesterNumber, int grade, typeControl type, String subject) {
-        return semesters.get(semesterNumber - 1).addGradeSemester(grade, type, subject);
+        return semesters.get(semesterNumber - 1).
+            addGradeSemester(grade, type, subject);
     }
 
+    /**Arithmetic medium.
+     *
+     * @return just double
+     *
+     */
     public double averageGrade() {
-        int totalMarks = semesters.stream().mapToInt(Semester::getMarksCnt).sum();
-        int totalValueMarks = semesters.stream().mapToInt(Semester::getMarksValue).sum();
+        int totalMarks = semesters.stream().
+            mapToInt(Semester::getMarksCnt).sum();
+        int totalValueMarks = semesters.stream().
+            mapToInt(Semester::getMarksValue).sum();
         if (totalMarks == 0) {
             return 0;
         }
         return (double) totalValueMarks / totalMarks;
     }
 
+    /**Checks can person go to free education.
+     *
+     * @param currentSemester number of current semester
+     *
+     * @return true if person can, false otherwise
+     *
+     */
     public boolean checkPaidFree(int currentSemester) {
         if (currentSemester == 1 || currentSemester == 2) {
             return false;
         }
-        return (semesters.get(currentSemester - 3).checkFinalMarks(4, 3) && semesters.get(currentSemester - 2).checkFinalMarks(4, 3));
+        return (semesters.get(currentSemester - 3).
+            checkFinalMarks(4, 3) && semesters.
+            get(currentSemester - 2).checkFinalMarks(4, 3));
     }
 
+    /**Checks if person can get red diploma.
+     *
+     * @param currentSemester number of current semester
+     *
+     * @return true if person can, false otherwise
+     *
+     */
     public boolean checkRedDiploma(int currentSemester) {
-        int totalMarks = semesters.stream().limit(currentSemester).mapToInt(Semester::getMarksCnt).sum();
-        int totalFiveMarks = semesters.stream().limit(currentSemester).mapToInt(Semester::getFiveMarksCnt).sum();
+        int totalMarks = semesters.stream().
+            limit(currentSemester).mapToInt(Semester::getMarksCnt).sum();
+        int totalFiveMarks = semesters.stream().
+            limit(currentSemester).mapToInt(Semester::getFiveMarksCnt).sum();
         if (currentSemester == 8) {
             if (semesters.get(7).getMarksValue() != 5) {
                 return false;
@@ -55,17 +100,34 @@ public class ElectronicBook {
         return !(((double) totalFiveMarks / totalMarks) < 0.75);
     }
 
+    /**Checks if person can get increased scholarship.
+     *
+     * @param currentSemester number of current semester
+     *
+     * @return true if person can, false otherwise
+     *
+     */
     public boolean increasedScholarship(int currentSemester) {
         if (currentSemester == 1 || currentSemester == 2) {
             return false;
         }
-        return (semesters.get(currentSemester - 3).checkFinalMarks(5, 5) && semesters.get(currentSemester - 2).checkFinalMarks(5, 5));
+        return (semesters.get(currentSemester - 3).
+            checkFinalMarks(5, 5) && semesters.
+            get(currentSemester - 2).checkFinalMarks(5, 5));
     }
 
+    /**Just getter for semesters.
+     *
+     * @return list of semesters
+     *
+     */
     public List<Semester> getSemesters() {
         return semesters;
     }
 
+    /**
+     * Enum class for types of assignments.
+     */
     public enum typeControl {
         TASK,
         TEST,
--- Task_1_4_1/src/main/java/ru/nsu/zarcer/Semester.java
+++ Task_1_4_1/src/main/java/ru/nsu/zarcer/Semester.java
@@ -3,11 +3,35 @@ package ru.nsu.zarcer;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class for semester.
+ */
 public class Semester {
 
     private Map<ElectronicBook.typeControl, Grades> records;
 
-    public Semester(int taskRestriction, int testRestriction, int colloquiumRestriction, int examRestriction, int diffCreditRestriction, int creditRestriction, int practiceDefenceRestriction, int vkrRestriction) {
+    /**Constructor with restrictions per semester.
+     *
+     * @param taskRestriction restriction
+     *
+     * @param testRestriction restriction
+     *
+     * @param colloquiumRestriction restriction
+     *
+     * @param examRestriction restriction
+     *
+     * @param diffCreditRestriction restriction
+     *
+     * @param creditRestriction restriction
+     *
+     * @param practiceDefenceRestriction restriction
+     *
+     * @param vkrRestriction restriction
+     *
+     */
+    public Semester(int taskRestriction, int testRestriction, int colloquiumRestriction, int examRestriction,
+                    int diffCreditRestriction, int creditRestriction,
+                    int practiceDefenceRestriction, int vkrRestriction) {
         records = new HashMap<>();
         records.put(ElectronicBook.typeControl.TASK, new Grades(taskRestriction));
         records.put(ElectronicBook.typeControl.TEST, new Grades(testRestriction));
@@ -19,26 +43,61 @@ public class Semester {
         records.put(ElectronicBook.typeControl.VKR, new Grades(vkrRestriction));
     }
 
+    /**Adds grade to book, shell.
+     *
+     * @param grade from 1 to 5
+     *
+     * @param type type of assignment
+     *
+     * @param subject type of subject
+     *
+     * @return true if successful, false otherwise
+     *
+     */
     public boolean addGradeSemester(int grade, ElectronicBook.typeControl type, String subject) {
         return records.get(type).addGrade(grade, subject);
     }
 
+    /**Getter for marks cnt in this semester.
+     *
+     * @return just int
+     *
+     */
     public int getMarksCnt() {
         return records.values().stream().mapToInt(Grades::getMarksCnt).sum();
     }
 
+    /**Getter for five marks cnt in this semester.
+     *
+     * @return just int
+     */
     public int getFiveMarksCnt() {
         return records.values().stream().mapToInt(Grades::getFiveMarksCnt).sum();
     }
 
+    /**Getter for marks value in this semester.
+     *
+     * @return just int
+     */
     public int getMarksValue() {
         return records.values().stream().mapToInt(Grades::getMarksValue).sum();
     }
 
+    /**Checks exam and diff credit values, because they are final usually.
+     *
+     * @param exam mark should be at least that number
+     *
+     * @param diffCredit mark should be at least that number
+     *
+     * @return true if they satisfied arguments
+     *
+     */
     public boolean checkFinalMarks(int exam, int diffCredit) {
-        if (!records.get(ElectronicBook.typeControl.EXAM).getGrades().stream().allMatch(s -> s >= exam)) {
+        if (!records.get(ElectronicBook.typeControl.EXAM).
+            getGrades().stream().allMatch(s -> s >= exam)) {
             return false;
         }
-        return records.get(ElectronicBook.typeControl.DIFF_CREDIT).getGrades().stream().allMatch(s -> s >= exam);
+        return records.get(ElectronicBook.typeControl.DIFF_CREDIT).
+            getGrades().stream().allMatch(s -> s >= exam);
     }
 }
