3ea5bb68e06375f758b03c6f47ff3011bf431410
Zarcer <Zarcer@users.noreply.github.com>
2024-10-13 18:16:28 +0000

    Deploying to gh-pages from @ Zarcer/OOP@5ba8ebed385bc3407e41d1daa3ae87383d29f037 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.zarcer","l":"ElectronicBook"},{"p":"ru.nsu.zarcer","l":"Semester"},{"p":"ru.nsu.zarcer","l":"ElectronicBook.typeControl"}]
