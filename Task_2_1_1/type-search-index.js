dfca5729a0d85d872b9094d2a38fa812f7a12451
Zarcer <Zarcer@users.noreply.github.com>
2025-02-08 13:53:05 +0000

    Deploying to gh-pages from @ Zarcer/OOP@f955b15a4ef19c2e3e967c0cf9d223f00b1307cc 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.zarcer","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.zarcer","l":"NotPrimeFinder"},{"p":"ru.nsu.zarcer","l":"PrimeCore"},{"p":"ru.nsu.zarcer","l":"PrimeOperations"}];updateSearchResults();
