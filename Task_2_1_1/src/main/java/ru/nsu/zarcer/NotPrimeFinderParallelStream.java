53258115b6b0ce76bd6b3ce668ee20902053a02a
Zarc <rorer232323@gmail.com>
2025-02-11 23:16:32 +0700

    v6

--- Task_2_1_1/src/main/java/ru/nsu/zarcer/NotPrimeFinderParallelStream.java
+++ Task_2_1_1/src/main/java/ru/nsu/zarcer/NotPrimeFinderParallelStream.java
@@ -1,17 +1,10 @@
 package ru.nsu.zarcer;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.stream.Collectors;
+
 
 public class NotPrimeFinderParallelStream implements PrimeFinderInterface{
     public boolean checkNonPrime(int[] numbersArray){
-        ArrayDeque<Integer> numbers = Arrays.stream(numbersArray).boxed().collect(Collectors.toCollection(ArrayDeque::new));
-        for(Object item : numbers.parallelStream().map(PrimeOperations::isNotPrime).toArray()){
-            if((Boolean)item){
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(numbersArray).parallel().anyMatch(PrimeOperations::isNotPrime);
     }
 }
