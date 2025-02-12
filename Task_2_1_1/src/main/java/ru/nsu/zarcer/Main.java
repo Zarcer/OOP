a2dc03fa496782369589a8c809560a0d0eb6ba01
Zarc <rorer232323@gmail.com>
2025-02-12 13:34:16 +0700

    dog

--- Task_2_1_1/src/main/java/ru/nsu/zarcer/Main.java
+++ Task_2_1_1/src/main/java/ru/nsu/zarcer/Main.java
@@ -1,6 +1,13 @@
 package ru.nsu.zarcer;
 
+/**
+ * Just main class
+ */
 public class Main {
+    /**Default main
+     *
+     * @param args didn't use here
+     */
     public static void main(String[] args) {
         int[] workingArray = PrimeOperations.primeGenerator(5000000);
         System.out.println("ParallelsSteam " + PrimeOperations.findNonPrimeCountSpeed(new NotPrimeFinderParallelStream(), workingArray));
--- Task_2_1_1/src/main/java/ru/nsu/zarcer/NotPrimeFinderConsistent.java
+++ Task_2_1_1/src/main/java/ru/nsu/zarcer/NotPrimeFinderConsistent.java
@@ -1,6 +1,15 @@
 package ru.nsu.zarcer;
 
+/**
+ * Class for ordinary computing non-prime numbers.
+ */
 public class NotPrimeFinderConsistent implements PrimeFinderInterface {
+    /** Method for finding non-prime number
+     *
+     * @param numbers int array
+     *
+     * @return boolean, true if array has non-prime number, false otherwise
+     */
     @Override
     public boolean checkNonPrime(int[] numbers) {
         for (int number : numbers) {
