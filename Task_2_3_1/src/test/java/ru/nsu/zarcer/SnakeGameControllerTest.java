db72ad89127e96e2e4b4450da15c16d474fee3e1
Zarc <rorer232323@gmail.com>
2025-03-31 01:28:38 +0700

    v2.2

---
+++ Task_2_3_1/src/test/java/ru/nsu/zarcer/SnakeGameControllerTest.java
@@ -0,0 +1,14 @@
+package ru.nsu.zarcer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class SnakeGameControllerTest {
+//    @Test
+//    public void startGameLoop(){
+//        SnakeGameController controller = new SnakeGameController();
+//        controller.startGameLoop();
+//        assertTrue(true);
+//    }
+
+}
